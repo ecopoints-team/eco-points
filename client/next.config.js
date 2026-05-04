ce50081fb3856d141e06f1a35faa19e94bf8a516
John Paul Elias <121612097+jaypiasco@users.noreply.github.com>
2026-05-04 13:26:14 +0800

    chore: changed next.config.js for deployment

--- client/next.config.js
+++ client/next.config.js
@@ -13,10 +13,11 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 const nextConfig = {
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5000";
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:5000/api/:path*",
+        destination: `${apiUrl}/api/:path*`,
       },
     ];
   },
